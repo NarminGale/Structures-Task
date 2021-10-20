095c2fb82eb39f0d51873c2c1033aeb3273a18aa
NarminGale <narmingale@gmail.com>
2021-10-20 22:45:09 +0400

    Imported ReactRouter

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import ReactRouter from './router'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactRouter />
   </React.StrictMode>,
   document.getElementById('app')
 )
